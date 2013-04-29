ac198df2f4215d8cdf31f877eb913d4e5093b53f
mikemoraned <mike@houseofmoran.com>
2013-04-29 22:07:35 +0100

    load Trello api

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,26 @@
 require.config({
     paths: {
         jquery: '../components/jquery/jquery',
-        bootstrap: 'vendor/bootstrap'
+        'jquery-migrate': '../components/jquery/jquery-migrate',
+        bootstrap: 'vendor/bootstrap',
+        trello: 'https://api.trello.com/1/client.js?key=afaec27e30009b0b1cfb14d85f384ee1'
     },
     shim: {
         bootstrap: {
             deps: ['jquery'],
             exports: 'jquery'
+        },
+        trello: {
+            deps: ['jquery', 'jquery-migrate'],
+            exports: 'Trello'
         }
     }
 });
 
-require(['app', 'jquery', 'bootstrap'], function (app, $) {
+require(['app', 'jquery', 'trello', 'bootstrap'], function (app, $, Trello) {
     'use strict';
     // use app here
     console.log(app);
     console.log('Running jQuery %s', $().jquery);
+    console.log('Trello: %s', Trello);
 });
