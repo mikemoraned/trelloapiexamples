e67ebf638829741210f8d8d8077a10951edc7a83
mikemoraned <mike@houseofmoran.com>
2013-04-29 23:32:51 +0100

    show status

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,30 +1,38 @@
 /*global define */
-define(['ko'], function (ko) {
+define(['ko', 'trello'], function (ko, Trello) {
     'use strict';
 
-    var loggedIn = ko.observable(false);
+    var LOGGED_IN = "logged in";
+    var LOGGING_IN = "logging in";
+    var LOGGED_OUT = "logged out";
+
+    var status = ko.observable(LOGGED_OUT);
 
     return {
+
+        "status": status,
 
         "login" : {
             "do" : function() {
                 console.log("Log in");
-                loggedIn(true);
+                status(LOGGING_IN);
+
+                status(LOGGED_IN);
             },
 
             "can" : ko.computed(function() {
-                return !loggedIn();
+                return status() === LOGGED_OUT;
             })
         },
 
         "logout" : {
             "do" : function() {
                 console.log("Log out");
-                loggedIn(false);
+                status(LOGGED_OUT);
             },
 
             "can" : ko.computed(function() {
-                return loggedIn();
+                return status() === LOGGED_IN;
             })
         }
     };
