02b202328170f0c7e8812d2792cd70b79dd6f12c
mikemoraned <mike@houseofmoran.com>
2013-05-02 22:37:17 +0100

    fetch actions too

--- app/scripts/member.js
+++ app/scripts/member.js
@@ -7,8 +7,15 @@ define(['ko', 'trello', 'statuses'], function (ko, Trello, STATUSES) {
         this.members = ko.observable(data.memberships.length);
     }
 
+    function Action(data) {
+        this.name = ko.observable(data.type);
+    }
+
+    var MAX_ROWS = 8;
+
     var Member = function(auth, errors) {
         var boards = ko.observableArray();
+        var actions = ko.observableArray();
         return {
             "fetchBoards" : {
                 "do" : function() {
@@ -30,12 +37,41 @@ define(['ko', 'trello', 'statuses'], function (ko, Trello, STATUSES) {
                 })
             },
 
+            "fetchActions" : {
+                "do" : function() {
+                    actions([]);
+                    Trello.members.get("me/actions",
+                        function (results) {
+                            results.forEach(function(result) {
+                                actions.push(new Action(result));
+                            });
+                        },
+                        function (error) {
+                            errors.onError(error);
+                        }
+                    );
+                },
+
+                "can" : ko.computed(function() {
+                    return auth.status() === STATUSES.LOGGED_IN;
+                })
+            },
+
             boards: ko.computed(function() {
-                if (boards().length > 4) {
-                    return boards().slice(0, 4);
+                if (boards().length > MAX_ROWS) {
+                    return boards().slice(0, MAX_ROWS);
                 }
                 else {
                     return boards();
+                }
+            }),
+
+            actions: ko.computed(function() {
+                if (actions().length > MAX_ROWS) {
+                    return actions().slice(0, MAX_ROWS);
+                }
+                else {
+                    return actions();
                 }
             })
         };
